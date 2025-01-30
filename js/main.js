2da8b5a647bd24d444210a89ee8e13b1c88866c8
juaniprogramador <juani.trejo07@gmail.com>
2025-01-30 04:56:40 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('¿Quieres ser mi San valentin tontita? TE AMO 💓').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
